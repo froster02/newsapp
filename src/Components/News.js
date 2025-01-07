3ab9d6ac7d13b479120cd3e2d847dfc17065ed3c
Arush Naudiyal <arushnaudiyal@gmail.com>
2025-01-07 22:12:06 +0530

    add Navbar component and update App structure; include web-vitals dependency

---
+++ src/Components/News.js
@@ -0,0 +1,13 @@
+import react, { Components } from 'react'
+
+export class News extends Components {
+    render() {
+        return (
+            <div>
+                <h1>News Component</h1>
+            </div>
+        )
+    }
+}
+
+export default News
