bed869846d2fab809609fd37f3adae0d2e88b2ec
Arush Naudiyal <arushnaudiyal@gmail.com>
2025-01-08 22:39:40 +0530

    add News component and sample output data; update App structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import Navbar from './Components/NavBar.js'
 import './App.css';
+import News from './Components/News.js';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Navbar title="NewsMonkey" aboutText="About NewsMonkey" />
+        <News />
       </div>
     )
   }
---
+++ src/Components/NewsItems.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class NewsItems extends Component {
+    render() {
+        let { title, description, newsUrl } = this.props;
+        return (
+            <div className="card h-100">
+                <div className="card-body d-flex flex-column">
+                    <h5 className="card-title" style={{ height: "48px", overflow: "hidden" }}>{title}</h5>
+                    <p className="card-text" style={{ height: "120px", overflow: "hidden" }}>{description}</p>
+                    <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark mt-auto">Read More</a>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default NewsItems
