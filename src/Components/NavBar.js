ae8dfa288e4766e152e1fa8d4a4474233ad5943b
Arush Naudiyal <arushnaudiyal@gmail.com>
2025-01-08 16:05:58 +0530

    update Navbar component to change brand name from "NEWS App" to "NEWS_App"

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -11,7 +11,7 @@ export class navbar extends Component {
             <div>
                 <div>
                     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                        <a className="navbar-brand" href="/" style={{ marginLeft: '20px' }}>NEWS App</a>
+                        <a className="navbar-brand" href="/" style={{ marginLeft: '20px' }}>NEWS_App</a>
                         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
