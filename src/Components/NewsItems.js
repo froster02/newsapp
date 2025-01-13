40a5a5dff2b2851a9b4a8616573bf176e07edc83
Arush Naudiyal <arushnaudiyal@gmail.com>
2025-01-13 23:39:26 +0530

    refactor NewsItems component to remove unused newsUrl prop and update Read More link

--- src/Components/NewsItems.js
+++ src/Components/NewsItems.js
@@ -2,13 +2,12 @@ import React, { Component } from 'react'
 
 export class NewsItems extends Component {
     render() {
-        let { title, description, newsUrl } = this.props;
+        let { title, description } = this.props;
         return (
             <div className="card h-100">
                 <div className="card-body d-flex flex-column">
                     <h5 className="card-title" style={{ height: "48px", overflow: "hidden" }}>{title}</h5>
                     <p className="card-text" style={{ height: "120px", overflow: "hidden" }}>{description}</p>
-                    <a href="noreferrer" target="_blank" className="btn btn-sm btn-dark mt-auto">Read More</a>
                 </div>
             </div>
         )
