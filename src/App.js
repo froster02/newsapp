c421a2744d65c07c72f0e5d39ad61f07c43b1e06
Arush Naudiyal <arushnaudiyal@gmail.com>
2025-01-19 23:33:16 +0530

    add react-infinite-scroll-component and react-top-loading-bar; implement loading bar in App component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import Navbar from './Components/NavBar.js'
 import './App.css';
 import News from './Components/News.js';
+import LoadingBar from "react-top-loading-bar";
+
 import {
   BrowserRouter as Router,
   Routes,
@@ -10,11 +12,19 @@ import {
 
 
 export default class App extends Component {
+  setProgress = (progress) => {
+    this.setState({ progress: progress });
+  }
   render() {
     return (
       <div>
         <Router>
           <Navbar title="News App" aboutText="About" />
+
+          <LoadingBar
+            color="#f11946"
+            progress={10}
+          />
           <Routes>
             <Route exact path="/" element={<News key="business" country="us" category="business" />} />
             <Route exact path="/business" element={<News key="business" country="us" category="business" />} />
