f22b3433f7facbf2c60a8aa65c97a1efecf00434
Arush Naudiyal <arushnaudiyal@gmail.com>
2025-02-04 23:22:19 +0530

    convert NavBar component from class-based to functional component

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,47 +2,42 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 // converting class based component to functional component
-export class navbar extends Component {
-    static propTypes = {
+const navbar = () => {
 
-    }
-
-    render() {
-        return (
+    return (
+        <div>
             <div>
-                <div>
-                    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                        <Link className="navbar-brand" to="/" style={{ marginLeft: '20px' }}>NEWS App</Link>
-                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav mr-auto">
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/">Home</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/business">business</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link " to="/entertainment">entertainment</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link " to="/health">health</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/sports">sports</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link " to="/science">science</Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
-                </div >
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <Link className="navbar-brand" to="/" style={{ marginLeft: '20px' }}>NEWS App</Link>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/business">business</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link " to="/entertainment">entertainment</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link " to="/health">health</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/sports">sports</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link " to="/science">science</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
             </div >
-        )
-    }
+        </div >
+    )
 }
 
 export default navbar
