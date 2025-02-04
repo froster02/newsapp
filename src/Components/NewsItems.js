ab235a94fed47235470a634557cc0ede6b48006b
Arush Naudiyal <arushnaudiyal@gmail.com>
2025-02-04 23:28:32 +0530

    convert NewsItems component from class-based to functional component

--- src/Components/NewsItems.js
+++ src/Components/NewsItems.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class NewsItems extends Component {
-    render() {
-        let { title, description } = this.props;
-        return (
-            <div className="card h-100">
-                <div className="card-body d-flex flex-column">
-                    <h5 className="card-title" style={{ height: "48px", overflow: "hidden" }}>{title}</h5>
-                    <p className="card-text" style={{ height: "120px", overflow: "hidden" }}>{description}</p>
-                </div>
+
+const NewsItems = (props) => {
+    let { title, description } = props;
+    return (
+        <div className="card h-100">
+            <div className="card-body d-flex flex-column">
+                <h5 className="card-title" style={{ height: "48px", overflow: "hidden" }}>{title}</h5>
+                <p className="card-text" style={{ height: "120px", overflow: "hidden" }}>{description}</p>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default NewsItems
